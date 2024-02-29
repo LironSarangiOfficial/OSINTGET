d7918863de2b66a00c810e258570fa9d7d27c416
Liron <sarangi.liron7@gmail.com>
2024-03-01 00:21:37 +0530

    basic structure

---
+++ osintget/functionalities/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+def iplocation():
+    pass
+
+def fraudEmail():
+    pass
+
+def phoneOSINT():
+    pass
+
+def fraudLink():
+    pass
+
+
---
+++ osintget/osintget/views.py
@@ -0,0 +1,9 @@
+# creator = LIRON SARANGI
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
